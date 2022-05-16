bf39f449d491bf3385f28293d0f6097c57df3590
Jaxcoder <codenamejason@outlook.com>
2022-05-16 14:34:07 -0400

    dashboard

--- packages/react-app/src/views/Dashboard.jsx
+++ packages/react-app/src/views/Dashboard.jsx
@@ -5,7 +5,9 @@ const Dashboard = ({ readContracts, writeContracts, address, tx, ...props }) => 
   return (
     <div>
       <Row>
-        <Col span={24}>Testing</Col>
+        <Col span={8}>Testing</Col>
+        <Col span={8}></Col>
+        <Col span={8}></Col>
       </Row>
     </div>
   );
