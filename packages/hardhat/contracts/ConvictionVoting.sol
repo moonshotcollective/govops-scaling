b9aa2e64163ca2266dfa920ba39b95ed932469f9
Jaxcoder <codenamejason@outlook.com>
2022-05-16 15:03:34 -0400

    update pragma after hh install globaly

--- packages/hardhat/contracts/ConvictionVoting.sol
+++ packages/hardhat/contracts/ConvictionVoting.sol
@@ -23,7 +23,7 @@
                     Moonshot Collective
             https://github.com/moonshotcollective
 */
-pragma solidity 0.8.4;
+pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
