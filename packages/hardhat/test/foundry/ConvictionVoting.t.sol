2e608ea0c3a8cff98165357b1a094a3a7a1f68c8
Jaxcoder <codenamejason@outlook.com>
2022-05-19 09:01:38 -0400

    test update

--- packages/hardhat/test/foundry/ConvictionVoting.t.sol
+++ packages/hardhat/test/foundry/ConvictionVoting.t.sol
@@ -6,9 +6,10 @@ import "forge-std/Test.sol";
 import {ConvictionVoting} from "contracts/ConvictionVoting.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "contracts/libs/ABDKMath64x64.sol";
 
-contract ConvictionVotingTest is Test {
+contract ConvictionVotingTest is Test, Ownable {
     using SafeERC20 for IERC20;
 
     ConvictionVoting private convictionVoting;
@@ -16,6 +17,7 @@ contract ConvictionVotingTest is Test {
     IERC20 token;
 
     error BadGaugeId();
+    error EmptyCount();
 
     event NewGauge(uint256 indexed id);
     event AddConviction(
@@ -53,6 +55,14 @@ contract ConvictionVotingTest is Test {
 
     mapping(uint256 => Gauge) public gauges;
 
+    function testAddGauge() external {
+        uint256 current = ++currentGaugeId;
+        Gauge storage gauge = gauges[current]; // gauges start from 1...
+        gauge.id = current;
+
+        emit NewGauge(current);
+    }
+
     function testAddConviction(
         address user,
         uint256 gaugeId,
@@ -71,14 +81,6 @@ contract ConvictionVotingTest is Test {
         emit AddConviction(gaugeId, convictionId, user, amount);
     }
 
-    function testAddGauge() external {
-        uint256 current = ++currentGaugeId;
-        Gauge storage gauge = gauges[current]; // gauges start from 1...
-        gauge.id = current;
-
-        emit NewGauge(current);
-    }
-
     function testGetConvictionScore(uint256 gaugeId)
         external
         view
@@ -95,4 +97,49 @@ contract ConvictionVotingTest is Test {
 
         return score;
     }
+
+    /// @notice index out of bounds error
+    // function testRemoveConvictionByIds(
+    //     uint256 gaugeId,
+    //     uint256 count,
+    //     bool oldestFirst,
+    //     uint256[] calldata convictions
+    // ) external {
+    //     Gauge storage gauge = gauges[gaugeId];
+    //     if (gauge.id != 0) revert BadGaugeId();
+    //     if (count != 0) revert EmptyCount();
+    //     uint256 returnAmount = 0;
+    //     if (oldestFirst) {
+    //         for (uint256 i = 0; i <= count; i++) {
+    //             require(
+    //                 gauge.convictions[convictions[i]].userAddress ==
+    //                     _msgSender(),
+    //                 "ONLY_VOTER"
+    //             );
+    //             returnAmount += gauge.convictions[convictions[i]].amount;
+    //             delete gauge.convictions[convictions[i]];
+    //         }
+    //         gauge.convictionsByUser[_msgSender()] = uint256[](
+    //             convictions[:count]
+    //         );
+    //     } else {
+    //         for (
+    //             uint256 i = convictions.length - 1;
+    //             i >= convictions.length - count;
+    //             i--
+    //         ) {
+    //             require(
+    //                 gauge.convictions[convictions[i]].userAddress ==
+    //                     _msgSender(),
+    //                 "ONLY_VOTER"
+    //             );
+    //             returnAmount += gauge.convictions[convictions[i]].amount;
+    //             delete gauge.convictions[convictions[i]];
+    //         }
+    //         gauge.convictionsByUser[_msgSender()] = uint256[](
+    //             convictions[:count]
+    //         );
+    //     }
+    //     // token.safeTransfer(_msgSender(), returnAmount);
+    // }
 }
