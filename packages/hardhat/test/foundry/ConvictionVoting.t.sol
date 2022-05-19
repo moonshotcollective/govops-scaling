d36af2e95ed58283014fd2df20e67cf050397115
Jaxcoder <codenamejason@outlook.com>
2022-05-18 21:46:29 -0400

    update tests file

--- packages/hardhat/test/foundry/ConvictionVoting.t.sol
+++ packages/hardhat/test/foundry/ConvictionVoting.t.sol
@@ -4,9 +4,18 @@ pragma solidity >=0.8.0 <0.9.0;
 import "forge-std/Test.sol";
 
 import {ConvictionVoting} from "contracts/ConvictionVoting.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "contracts/libs/ABDKMath64x64.sol";
 
 contract ConvictionVotingTest is Test {
+    using SafeERC20 for IERC20;
+
     ConvictionVoting private convictionVoting;
+
+    IERC20 token;
+
+    error BadGaugeId();
 
     event NewGauge(uint256 indexed id);
     event AddConviction(
@@ -44,6 +53,24 @@ contract ConvictionVotingTest is Test {
 
     mapping(uint256 => Gauge) public gauges;
 
+    // function testAddConviction(
+    //     address user,
+    //     uint256 gaugeId,
+    //     uint256 amount
+    // ) external {
+    //     Gauge storage gauge = gauges[gaugeId];
+    //     if (gauge.id != 0) revert BadGaugeId();
+    //     uint256 convictionId = gauge.currentConvictionId++; // convictionId starts from 0...
+    //     Conviction storage convictions = gauge.convictions[convictionId];
+    //     convictions.userAddress = user;
+    //     convictions.amount = amount;
+    //     convictions.timestamp = block.timestamp;
+    //     gauge.convictionsByUser[user].push(convictionId);
+    //     token.safeTransferFrom(user, address(this), amount);
+
+    //     emit AddConviction(gaugeId, convictionId, user, amount);
+    // }
+
     function testAddGauge() external {
         uint256 current = ++currentGaugeId;
         Gauge storage gauge = gauges[current]; // gauges start from 1...
@@ -52,5 +79,20 @@ contract ConvictionVotingTest is Test {
         emit NewGauge(current);
     }
 
-    // ...
+    function testGetConvictionScore(uint256 gaugeId)
+        external
+        view
+        returns (uint256 score)
+    {
+        Gauge storage gauge = gauges[gaugeId];
+        for (uint256 i = 0; i < gauge.currentConvictionId; i++) {
+            uint256 x1 = uint256(
+                ABDKMath64x64.sqrtu(gauge.convictions[i].amount)
+            );
+            uint256 x2 = (block.timestamp - gauge.convictions[i].timestamp)**2;
+            score += x1 * x2;
+        }
+
+        return score;
+    }
 }
