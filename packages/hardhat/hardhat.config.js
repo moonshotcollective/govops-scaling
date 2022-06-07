2fe43c7c8c95b4b5cc2afd38a6b7589e53898aaa
qedk <1994constant@gmail.com>
2022-06-07 22:43:02 +0400

    feat: Bump versions

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -153,7 +153,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.13",
+        version: "0.8.14",
         settings: {
           optimizer: {
             enabled: true,
