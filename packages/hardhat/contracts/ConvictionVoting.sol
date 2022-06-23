d79eb5fca45070bfbef85bd18083289ebee49461
qedk <1994constant@gmail.com>
2022-06-24 00:38:46 +0400

    feat: Bump solidity version

--- packages/hardhat/contracts/ConvictionVoting.sol
+++ packages/hardhat/contracts/ConvictionVoting.sol
@@ -23,7 +23,7 @@
                     Moonshot Collective
             https://github.com/moonshotcollective
 */
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
